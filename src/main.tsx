5adc7876a5009297091428eb41d17c286d5d3486
VitorHugoCD <vitordelantonia@gmail.com>
2024-09-04 18:21:46 -0300

    feat: conditional (base)

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Button } from '@B-UI/components'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const App = () => {
+  return (
     <Button 
       color='green'
       size='medium'
       label='Botao Legal :)'
     />
+  )
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
   </StrictMode>,
 )
